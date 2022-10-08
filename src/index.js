c22adeb6a0be586c0eb48265aa3da01bffc1ba3d
Benjamin <benjamin.manguet@negocian.fr>
2022-10-08 11:24:57 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// Exercice 4
+// Ecris ton Code ici
+
+// Exercice 4
+
+// ###################
+
+// Exercice 5
+// Ecris ton Code ici
+
+// Exercice 5
+
+// ###################
+
+// Exercice 6
+// Ecris ton Code ici
+
+// Exercice 6
+
+// ###################
+
+// Exercice 7
+// Ecris ton Code ici
+
+// Exercice 7
+
+// ###################
+
+// Exercice 8
+// Ecris ton Code ici
+
+// Exercice 8
